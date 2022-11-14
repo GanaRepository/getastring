d3d06abfd1f6ce7df9173d8577c3fbf29b66ca59
GanaRepository <mr.ganapathireddy@gmail.com>
2022-11-14 17:25:54 +0000

    get a string

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Express Js");
+});
+
+app.listen(3000);
+
+module.exports = app;
